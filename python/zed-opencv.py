1d0e72fd981c00d18a6ceee3e1ec34eaaaf81bba
Vishwesh Ravi Shrimali <vishweshshrimali5@gmail.com>
2021-02-04 14:46:57 +0530

    Update zed-opencv.py

--- python/zed-opencv.py
+++ python/zed-opencv.py
@@ -118,9 +118,6 @@ def main() :
     zed = sl.Camera()
 
     # Set configuration parameters
-    #input_type = sl.InputType()
-    #if len(sys.argv) >= 2 :
-    #    input_type.set_from_svo_file(sys.argv[1])
     init = sl.InitParameters()
     init.camera_resolution = sl.RESOLUTION.HD720
     init.depth_mode = sl.DEPTH_MODE.ULTRA
@@ -141,38 +138,18 @@ def main() :
     res.width = 720
     res.height = 404
     point_cloud = sl.Mat(res.width,res.height,sl.MAT_TYPE.F32_C4,sl.MEM.CPU)
-
-    # Set runtime parameters after opening the camera
-    #runtime = sl.RuntimeParameters()
-    #runtime.sensing_mode = sl.SENSING_MODE.STANDARD
-
-    # Prepare new image size to retrieve half-resolution images
-    #image_size = zed.get_camera_information().camera_resolution
-    #image_size.width = image_size.width /2
-    #image_size.height = image_size.height /2
-
-    # Declare your sl.Mat matrices
-    #image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
-    #depth_image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
-    #point_cloud = sl.Mat()
-
+    
     key = ' '
     while key != 113 :
         err = zed.grab()
         if err == sl.ERROR_CODE.SUCCESS :
-            # Retrieve the left image, depth image in the half-resolution
-            #zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
-            #zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, image_size)
-            # Retrieve the RGBA point cloud in half resolution
             zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA, sl.MEM.CPU, res)
 
             # To recover data from sl.Mat to use it with opencv, use the get_data() method
             # It returns a numpy array that can be used as a matrix with opencv
             image_ocv = res.get_data()
-            #depth_image_ocv = depth_image_zed.get_data()
 
             cv2.imshow("Image", image_ocv)
-            #cv2.imshow("Depth", depth_image_ocv)
 
             key = cv2.waitKey(10)
 
